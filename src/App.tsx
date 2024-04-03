ae2f2993ade4d161cf1ed92fa229dbd2ce8c96d6
ramosvi <vitor_hugo_di@hotmail.com>
2024-04-03 17:53:48 -0300

    feat: cria lista de tasks e configura vite

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState, FormEvent } from 'react'
+import { Task } from './Task'
 
 function App() {
+  const [newTast, setNewTask] = useState("");
+
+  const [tasks, setTasks] = useState([
+    {
+      id: "0",
+      name: "Estudar react com typescript"
+    },
+    {
+      id: "1",
+      name: "Inscrever no canal do Sujeito Programador"
+    },
+    {
+      id: "2",
+      name: "Pagar o aluguel"
+    },
+  ])
+
+
+  function handleAddTask(event: FormEvent) {
+    event.preventDefault();
+
+    if (newTast === "") return;
+
+    let newItem = {
+      id: `${tasks.length + 1}`,
+      name: newTast
+    }
+    setTasks(allTasks => [...allTasks, newItem])
+    setNewTask("")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="w-full h-screen flex flex-col items-center px-4 pt-52">
+      <h1 className="font-bold text-4xl text-white mb-4">Tarefas</h1>
+
+
+      <form className="w-full max-w-2xl mb-4 flex" onSubmit={handleAddTask}>
+        <input
+          type="text"
+          placeholder="Digite o nome da tarefa..."
+          className="flex-1 h-10 rounded-md px-2"
+          value={newTast}
+          onChange={(event) => setNewTask(event.target.value)}
+        />
+        <button
+          type="submit"
+          className="bg-blue-500 ml-4 rounded-md px-4 text-white font-medium"
         >
-          Learn React
-        </a>
-      </header>
+          Add
+        </button>
+      </form>
+
+      <section className="bg-zinc-100 p-3 rounded-md w-full max-w-2xl">
+        <article>
+          {tasks.map((task) => (
+            <Task key={task.id} task={task} />
+          ))}
+        </article>
+      </section>
+
+
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/task.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+interface TaskProps {
+    task: {
+      id: string;
+      name: string;
+    }
+  }
+  
+  export function Task({ task }: TaskProps) {
+    return (
+      <div
+        className="w-full bg-zinc-300 mb-2 last:mb-0 px-2 py-3 rounded border-[2px] border-zinc-400"
+      >
+        <p className="font-medium">{task.name}</p>
+      </div>
+    );
+  }
+  
